855a72bb416609665d4c267ece838adf0a7b7fcb
luis david <11556208+luisdamora@users.noreply.github.com>
2025-10-07 13:31:04 -0500

    feat: Actualizar botones de acción
    - Se reemplazó el icono `BanknoteArrowDown` por `HandCoins`.
    - Se actualizaron las clases de los botones para centrar el contenido.
    - Se aumentó el tamaño de los iconos y se modificó su margen.

--- src/components/modules/sales/cash-sale/components/action-buttons.tsx
+++ src/components/modules/sales/cash-sale/components/action-buttons.tsx
@@ -1,4 +1,5 @@
-import { BanknoteArrowDown, Droplet } from "lucide-react";
+import { Droplet, HandCoins } from "lucide-react";
+
 import React from "react";
 import { BUTTON_STYLES } from "@/lib/config/theme";
 
@@ -10,17 +11,23 @@ export const ActionButtons: React.FC<ActionButtonsProps> = ({
 	onTripleZero,
 }) => (
 	<div className="grid grid-cols-3 gap-3 max-w-xl mx-auto mb-4">
-		<button type="button" className={BUTTON_STYLES.primary}>
-			<BanknoteArrowDown className="w-4 h-4 mr-1" />$
+		<button
+			type="button"
+			className={`${BUTTON_STYLES.primary} flex items-center justify-center`}
+		>
+			<HandCoins className="w-6 h-6 mr-2" />$
 		</button>
-		<button type="button" className={BUTTON_STYLES.primary}>
-			<Droplet className="w-4 h-4 mr-1" />
+		<button
+			type="button"
+			className={`${BUTTON_STYLES.primary} flex items-center justify-center`}
+		>
+			<Droplet className="w-6 h-6 mr-2" />
 			Vol.
 		</button>
 		<button
 			type="button"
 			onClick={onTripleZero}
-			className={BUTTON_STYLES.primary}
+			className={`${BUTTON_STYLES.primary} flex items-center justify-center`}
 		>
 			x $1.000
 		</button>
